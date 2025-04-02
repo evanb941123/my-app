24b8e4b3aee2ebbc217255a71618011c3f820706
evanb941123 <evan.b.941123@gmail.com>
2025-04-02 19:32:11 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+//https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default App
